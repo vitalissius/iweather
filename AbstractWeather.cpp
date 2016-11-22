9fab0acd32e239db0a5380db352030adebcabbd4
Vitalii <vitaliy.sklyarov@gmail.com>
2016-11-23 01:12:17 +0200

    Add the header file AbstractWeather.h and the source file AbstractWeather.cpp

---
+++ AbstractWeather.cpp
@@ -0,0 +1,365 @@
+#include "AbstractWeather.h"
+
+/* Methods of the Astronomy additional private structure */
+
+AbstractWeather::Astronomy::Astronomy(const char* sunrise, const char* sunset)
+    : m_sunrise(sunrise)
+    , m_sunset(sunset)
+{
+    if (std::regex_search(sunrise, std::regex("[pPaA].??m|M.??")))
+        m_clockSystemUnit = ClockSystemUnits::THE12HOURCLOCK;
+    else
+        m_clockSystemUnit = ClockSystemUnits::THE24HOURCLOCK;
+}
+
+AbstractWeather::Astronomy::Astronomy()
+    : Astronomy("00:00", "00:00")
+{}
+
+void AbstractWeather::Astronomy::changeClockSystemTo(ClockSystemUnits clockSystem)
+{
+    if (m_clockSystemUnit == clockSystem)
+        return;
+
+    std::regex expr("([0-9]+):([0-9]+)");
+    std::smatch match;
+
+    for (auto* s : { &m_sunrise, &m_sunset })
+    {
+        if (std::regex_search(*s, match, expr))
+        {
+            int hours = std::stoi(match[1]);
+            int minuts = std::stoi(match[2]);
+
+            if (m_clockSystemUnit == ClockSystemUnits::THE12HOURCLOCK)
+            {
+                if (std::regex_search(*s, match, std::regex("(p.??m.??)")))
+                    *s = from12to24hourClock(hours, minuts, true);
+                else
+                    *s = from12to24hourClock(hours, minuts, false);
+            }
+            else
+            {
+                *s = from24to12hourClock(hours, minuts);
+            }
+        }
+    }
+
+    m_clockSystemUnit = clockSystem;
+}
+
+std::string AbstractWeather::Astronomy::from24to12hourClock(int hours, int minuts)
+{
+    std::string time = "";
+
+    if (hours <= 12)
+    {
+        if (hours == 0)
+            time += "12";
+        else
+            time += std::to_string(hours);
+    }
+    else
+    {
+        time += std::to_string(hours - 12);
+    }
+
+    time += ":";
+
+    if (minuts <= 9)
+        time += "0";
+    time += std::to_string(minuts);
+
+    if (hours <= 11)
+        time += " am";
+    else
+        time += " pm";
+    return time;
+}
+
+std::string AbstractWeather::Astronomy::from12to24hourClock(int hours, int minuts, bool isPm)
+{
+    std::string time = "";
+
+    if (isPm)
+    {
+        if (hours != 12)
+            hours += 12;
+    }
+    else if (hours == 12)
+        hours = 0;
+
+    if (hours <= 9)
+        time += "0";
+    time += std::to_string(hours);
+
+    time += ":";
+
+    if (minuts <= 9)
+        time += "0";
+    time += std::to_string(minuts);
+
+    return time;
+}
+
+
+
+/* Methods of the Forecast additional private structure */
+
+AbstractWeather::Forecast::Forecast(int code, std::string date, std::string day, int high, int low, std::string text)
+    : m_code(code)
+    , m_date(date)
+    , m_day(day)
+    , m_high(high)
+    , m_low(low)
+    , m_text(text)
+{}
+
+int AbstractWeather::Forecast::GetCode() const 
+{ 
+    return m_code; 
+}
+
+const std::string& AbstractWeather::Forecast::GetDate() const 
+{ 
+    return m_date; 
+}
+
+const std::string& AbstractWeather::Forecast::GetDayName() const 
+{ 
+    return m_day; 
+}
+
+int AbstractWeather::Forecast::GetHighTemp() const 
+{ 
+    return m_high;
+}
+
+int AbstractWeather::Forecast::GetLowTemp() const
+{
+    return m_low;
+}
+
+const std::string AbstractWeather::Forecast::GetDescriptionText() const
+{
+    return m_text;
+}
+
+
+
+/* AbstractWeather's manager methods */
+
+AbstractWeather::AbstractWeather()
+{
+    m_forecast.reserve(msc_forecastSize);
+}
+
+AbstractWeather::~AbstractWeather() = default;
+
+
+
+/* AbstractWeather's mutator methods */
+
+void AbstractWeather::AddForecast(int code, std::string date, std::string day, int high, int low, std::string text)
+{
+    static int index = 0;
+
+    if (index >= msc_forecastSize)
+    {
+        index = 0;
+        m_forecast.clear();
+    }
+    Forecast forecast(code, date, day, high, low, text);
+    m_forecast.emplace_back(std::move(forecast));
+    ++index;
+}
+
+void AbstractWeather::SetLocation(const char* city, const char* country, const char* region)
+{
+    m_location.m_city = city;
+    m_location.m_country = country;
+    m_location.m_region = region;
+}
+
+void AbstractWeather::SetCondition(int code, int temp, const char* text)
+{
+    m_condition.m_code = code;
+    m_condition.m_temp = temp;
+    m_condition.m_text = text;
+}
+
+void AbstractWeather::SetAstronomy(const char* sunrise, const char* sunset)
+{
+    m_astronomy = Astronomy(sunrise, sunset);
+}
+
+void AbstractWeather::SetAtmosphere(int humidity, float pressure, int pressureState)
+{
+    m_atmosphere.m_humidity = humidity;
+    m_atmosphere.m_pressure = pressure;
+    m_atmosphere.m_pressureState = units::PressureStateUnits(pressureState);
+}
+
+void AbstractWeather::SetWind(int direction, float speed)
+{
+    m_wind.m_direction = direction;
+    m_wind.m_speed = speed;
+}
+
+void AbstractWeather::SetPublicationDate(const char* pubDate)
+{
+    m_pubDate = pubDate;
+}
+
+
+
+/* AbstractWeather's accessor methods */
+#include <cassert>
+const std::vector<AbstractWeather::Forecast>& AbstractWeather::GetForecastVector() const
+{
+    if (msc_forecastSize != m_forecast.size())
+    {
+        std::stringstream ss;
+        ss << "Incorrect forecast size! Must be " << msc_forecastSize;
+        throw IncorrectForecastSize(ss.str().data());
+    }
+    return m_forecast;
+}
+
+const std::string& AbstractWeather::GetLocationCLine() const
+{
+    return m_location.m_city;
+}
+
+std::string AbstractWeather::GetLocationCcLine() const
+{
+    return m_location.m_city + ", " + m_location.m_country;
+}
+
+std::string AbstractWeather::GetLocationCcrLine() const
+{
+    return m_location.m_city + ", " + m_location.m_country + " (" + m_location.m_region + ")";
+}
+
+const std::string& AbstractWeather::GetWindDirectionLine() const
+{
+    float angleWithCorrection = m_wind.m_direction - 11.25f;
+    int index = static_cast<int>(std::ceil(angleWithCorrection / 22.5f)) % LanguagePack::csm_compassPoints;
+    units::CompassRoseUnits direction = static_cast<units::CompassRoseUnits>(index);
+    return m_langPack.GetCompassRoseName(direction);
+}
+
+std::string AbstractWeather::GetWindSpeedLine() const
+{
+    float speed = 0.0f;
+    switch (m_langPack.GetSpeedUnit())
+    {
+    case units::SpeedUnits::SU_KILOMETERSPERHOUR:
+        speed = to_KilometersPerHour();
+        break;
+    case units::SpeedUnits::SU_METERSPERSECOND:
+        speed = to_MetersPerSecond();
+        break;
+    case units::SpeedUnits::SU_MILESPERHOUR:
+        speed = to_MilesPerHour();
+        break;
+    case units::SpeedUnits::SU_FEETPERSECOND:
+        speed = to_FeetPerSecond();
+        break;
+    }
+    std::stringstream ss;
+    ss << std::fixed << std::setprecision(2) << speed;
+    return ss.str() + " " + m_langPack.GetSpeedName();
+}
+
+std::string AbstractWeather::GetHumidityLine() const
+{
+    return std::to_string(m_atmosphere.m_humidity) + " %";
+}
+
+std::string AbstractWeather::GetPressureLine() const
+{
+    float value = 0;
+    switch (m_langPack.GetPressureUnit())
+    {
+    case units::PressureUnits::PU_ATMOSPHERES:
+        value = to_Atm();
+        break;
+    case units::PressureUnits::PU_MILLIBARS:
+        value = to_MilliBar();
+        break;
+    case units::PressureUnits::PU_MILLIMETERSOFMERCURY:
+        value = to_MillimeterOfMercury();
+        break;
+    case units::PressureUnits::PU_INCHESOFMERCURY:
+        value = to_InchesOfMercury();
+        break;
+    }
+    std::stringstream ss;
+    ss << std::fixed << std::setprecision(2) << value;
+    return ss.str() + " " + m_langPack.GetPressureName();
+}
+
+const std::string& AbstractWeather::GetPressureStateLine() const
+{
+    auto pressureState = units::PressureStateUnits(m_atmosphere.m_pressureState);
+    return m_langPack.GetPressureStateName(pressureState);
+}
+
+const std::string& AbstractWeather::GetSunriseLine() const
+{
+    return m_astronomy.m_sunrise;
+}
+
+const std::string& AbstractWeather::GetSunsetLine() const
+{
+    return m_astronomy.m_sunset;
+}
+
+const std::string& AbstractWeather::GetPublicationDateLine() const
+{
+    return m_pubDate;
+}
+
+
+
+/* AbstractWeather's "changeror" methods */
+
+void AbstractWeather::ChangeSpeedUnitTo(units::SpeedUnits speedUnit)
+{
+    m_langPack.SetSpeedUnit(speedUnit);
+}
+
+void AbstractWeather::ChangePressureUnitTo(units::PressureUnits pressureUnit)
+{
+    m_langPack.SetPressureUnit(pressureUnit);
+}
+
+void AbstractWeather::ChangeLanguageTo(LanguagePack::LanguageUnits languageUnit)
+{
+    m_langPack.SetLanguageUnit(languageUnit);
+}
+
+void AbstractWeather::ChangeClockSystemTo24h()
+{
+    m_astronomy.changeClockSystemTo(Astronomy::ClockSystemUnits::THE24HOURCLOCK);
+}
+
+void AbstractWeather::ChangeClockSystemTo12h()
+{
+    m_astronomy.changeClockSystemTo(Astronomy::ClockSystemUnits::THE12HOURCLOCK);
+}
+
+
+
+/* Private additional AbstractWeather's "changeror" methods */
+
+float AbstractWeather::to_KilometersPerHour() const { return m_wind.m_speed; }
+float AbstractWeather::to_MetersPerSecond() const   { return m_wind.m_speed * 0.277778f; }
+float AbstractWeather::to_MilesPerHour() const      { return m_wind.m_speed * 0.621427f; }
+float AbstractWeather::to_FeetPerSecond() const     { return m_wind.m_speed * 0.911344f; }
+
+float AbstractWeather::to_Atm() const                   { return m_atmosphere.m_pressure; }
+float AbstractWeather::to_MilliBar() const              { return m_atmosphere.m_pressure * 1013.25f; }
+float AbstractWeather::to_MillimeterOfMercury() const   { return m_atmosphere.m_pressure * 760.000f; }
+float AbstractWeather::to_InchesOfMercury() const       { return m_atmosphere.m_pressure * 29.9213f; }
---
+++ AbstractWeather.h
@@ -0,0 +1,209 @@
+﻿#pragma once
+
+#include "Exceptions.h"
+#include "LanguagePack.h"
+
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <regex>
+#include <sstream>
+
+class AbstractWeather {
+private:
+    /* Additional private structures */
+
+    /* Additional private structure Location */
+    struct Location {
+        std::string m_city;
+        std::string m_country;
+        std::string m_region;
+    };
+
+    /* Additional private structure Wind */
+    struct Wind {
+        int m_direction;
+        float m_speed;
+    };
+
+    /* Additional private structure Atmosphere */
+    struct Atmosphere {
+        int m_humidity;
+        float m_pressure;
+        units::PressureStateUnits m_pressureState;
+    };
+
+    /* Additional private structure Astronomy */
+    struct Astronomy {
+		enum class ClockSystemUnits { THE12HOURCLOCK, THE24HOURCLOCK };
+
+        std::string m_sunrise;
+        std::string m_sunset;
+        ClockSystemUnits m_clockSystemUnit;
+
+    private:
+        std::string from24to12hourClock(int hours, int minuts);
+        std::string from12to24hourClock(int hours, int minuts, bool isPm);
+
+    public:
+        Astronomy(const char* sunrise, const char* sunset);
+        Astronomy();
+        void changeClockSystemTo(ClockSystemUnits clockSystem);
+    };
+
+    /* Additional private structure Condition */
+    struct Condition {
+        int m_code;
+        int m_temp;
+        std::string m_text;
+    };
+
+    /* Additional private structure Forecast */
+    struct Forecast {
+    private:
+        int m_code;
+        std::string m_date;
+        std::string m_day;
+        int m_high;
+        int m_low;
+        std::string m_text;
+
+    public:
+        Forecast(int code, std::string date, std::string day, int high, int low, std::string text);
+        int GetCode() const;
+        const std::string& GetDate() const;
+        const std::string& GetDayName() const;
+        int GetHighTemp() const;
+        int GetLowTemp() const;
+        const std::string GetDescriptionText() const;
+    };
+
+private:
+    /* AbstractWeather's private members */
+    std::vector<Forecast> m_forecast;
+    Location m_location;
+    Condition m_condition;
+    Astronomy m_astronomy;
+    Atmosphere m_atmosphere;
+    Wind m_wind;
+    std::string m_pubDate;
+    LanguagePack m_langPack;
+
+private:
+    /* AbstractWeather's private constant static member */
+    static const int msc_forecastSize = 5;
+
+public:
+    /* AbstractWeather's manager methods */
+    AbstractWeather();
+    virtual ~AbstractWeather() = 0;
+
+    /* AbstractWeather's mutator methods */
+    //@description: The weather forecast for a specific day
+    //@param: code - This code used to choose a text description or image for the forecast
+    //@param: date - The date to which this forecast applies
+    //@param: day - Day of the week to which this forecast applies (Mon, Tue, Wed, Thu, Fri, Sat, Sun)
+    //@param: high - The forecasted high temperature for this day (set this value by the Celsius)
+    //@param: low - The forecasted low temperature for this day (set this value by the Celsius)
+    //@param: text - A textual description of conditions (for example, "Partly Cloudy", "Sunny")
+    void AddForecast(int code, std::string date, std::string day, int high, int low, std::string text);
+    
+    //@description: The location of this forecast
+    //@param: city - City name
+    //@param: region - State, territory, or region, if given
+    //@param: country - Country name
+    void SetLocation(const char* city, const char* country, const char* region);
+
+    //@description: The current weather conditions
+    //@param: text - A textual description of conditions (for example, "Partly Cloudy", "Sunny")
+    //@param: code - This code used to choose a text description or image for the forecast
+    //@param: temp - The current temperature (set this value in the Celsius)
+    void SetCondition(int code, int temp, const char* text);
+    
+    //@description: Forecast information about current astronomical conditions
+    //@param: sunrise - Today's sunrise time
+    //@param: sunset - Today's sunset time
+    void SetAstronomy(const char* sunrise, const char* sunset);
+    
+    //@description: Forecast information about current atmospheric pressure and humidity
+    //@param: humidity - Humidity, in percent
+    //@param: pressure - Barometric pressure (set this value in the 'atm' units)
+    //@param: pressureState - State of the barometric pressure (steady=='0', rising=='1', falling=='2')
+    void SetAtmosphere(int humidity, float pressure, int pressureState);
+
+    //@description: Forecast information about wind
+    //@param: direction - Wind direction (set this value in the degrees from '0' to '359')
+    //@param: speed - Wind speed (set this value in the 'kph')
+    void SetWind(int direction, float speed);
+
+    //@description: Information about date and time when the forecast was posted
+    //@param: pubDate - The date and time (for example 'Tue, 22 Nov 2016 12:00 AM EET')
+    void SetPublicationDate(const char* pubDate);
+
+public:
+    /* AbstractWeather's accessor methods */
+    
+    //int GetConditionCode() const { return m_condition.m_code; }
+    //int GetConditionTemt() const { return m_condition.m_temp; }
+    //const std::string& GetConditionText() const { return m_condition.m_text; }
+
+    //@return: Ref to the vector of the Forecast-s (size of the vector must be equal msc_forecastSize)
+    const std::vector<Forecast>& GetForecastVector() const;
+
+    //@return: 'City'
+    const std::string& GetLocationCLine() const;
+
+    //@return: 'City, Country'
+    std::string GetLocationCcLine() const;
+
+    //@return: 'City, Country (Region)'
+    std::string GetLocationCcrLine() const;
+
+    //@return: Compass rose name
+    const std::string& GetWindDirectionLine() const;
+
+    //@return: String describing wind speed
+    std::string GetWindSpeedLine() const;
+
+    //@return: String describing humidity of the atmosphere
+    std::string GetHumidityLine() const;
+
+    //@return: String describing atmospheric pressure
+    std::string GetPressureLine() const;
+
+    //@return: String describing atmospheric pressure state
+    const std::string& GetPressureStateLine() const;
+
+    //@return: String describing time of the sunrise
+    const std::string& GetSunriseLine() const;
+
+    //@return: string describing time of the sunset
+    const std::string& GetSunsetLine() const;
+
+    //@return: String describing of the publication date (time)
+    const std::string& GetPublicationDateLine() const;
+
+public:
+    /* AbstractWeather's "changeror" methods */
+    void ChangeSpeedUnitTo(units::SpeedUnits speedUnit);
+    void ChangePressureUnitTo(units::PressureUnits pressureUnit);
+    void ChangeLanguageTo(LanguagePack::LanguageUnits languageUnit);
+
+    void ChangeClockSystemTo24h();
+    void ChangeClockSystemTo12h();
+
+private:
+    /* Private additional AbstractWeather's "changeror" methods */
+    //@return: Velocity in specific units
+    float to_KilometersPerHour() const;
+    float to_MetersPerSecond() const;
+    float to_MilesPerHour() const;
+    float to_FeetPerSecond() const;
+
+    //@return: Atmospheric pressure in specific units
+    float to_Atm() const;
+    float to_MilliBar() const;
+    float to_MillimeterOfMercury() const;
+    float to_InchesOfMercury() const;
+};

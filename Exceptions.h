91ff82d4dbbf803e3f2516a865e3985bc16201f1
Vitalii <vitaliy.sklyarov@gmail.com>
2016-12-08 00:25:49 +0200

    Added extra exceptions

--- Exceptions.h
+++ Exceptions.h
@@ -5,6 +5,7 @@
 
 class WeatherException : public std::exception {
 public:
+    WeatherException() = default;
     WeatherException(const char* message)
         : std::exception(message)
     {}
@@ -13,6 +14,28 @@ public:
 class IncorrectForecastSize : public WeatherException {
 public:
     IncorrectForecastSize(const char* message)
+        : WeatherException(message)
+    {}
+};
+
+class IncorrectTableName : public WeatherException {
+public:
+    IncorrectTableName() = default;
+    IncorrectTableName(const char* message)
+        : WeatherException(message)
+    {}
+};
+
+class SocketError : public WeatherException {
+public:
+    SocketError(const char* message)
+        : WeatherException(message)
+    {}
+};
+
+class IncorrectFile : public WeatherException {
+public:
+    IncorrectFile(const char* message)
         : WeatherException(message)
     {}
 };

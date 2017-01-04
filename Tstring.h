398190f76341712ba1d5c107160afd17c4dd1e28
Vitalii <vitaliy.sklyarov@gmail.com>
2017-01-04 04:42:33 +0200

    Add alias for std::basic_stringstream with TCHAR

--- Tstring.h
+++ Tstring.h
@@ -3,8 +3,10 @@
 #include <tchar.h>
 
 #include <string>
+#include <sstream>
 
 using tstring = std::basic_string<TCHAR, std::char_traits<TCHAR>, std::allocator<TCHAR>>;
+using tstringstream = std::basic_stringstream<TCHAR, std::char_traits<TCHAR>, std::allocator<TCHAR>>;
 
 template<typename T>
 tstring to_tstring(T t)

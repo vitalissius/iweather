b7e757b9eed5a53b4dc347bed915630d36d50eda
Vitalii <vitaliy.sklyarov@gmail.com>
2017-01-07 14:53:01 +0200

    Объединение взаимных сущностей

--- AppRegion.cpp
+++ AppRegion.cpp
@@ -1,5 +1,68 @@
 #include "AppRegion.h"
 
+namespace window_region
+{
+    void WindowRegion::reassign()
+    {
+        if (m_region)
+        {
+            DeleteObject(m_region);
+        }
+        m_region = CreateRoundRectRgn(m_x1, m_y1, m_x2, m_y2, m_roundw, m_roundh);
+    }
+
+    void WindowRegion::updateRound(int roundw, int roundh)
+    {
+        m_roundw = roundw;
+        m_roundh = roundh;
+    }
+
+    void WindowRegion::updateCoord(int x1, int y1, int x2, int y2)
+    {
+        m_x1 = x1;
+        m_y1 = y1;
+        m_x2 = x2;
+        m_y2 = y2;
+    }
+
+    WindowRegion::WindowRegion(int x1, int y1, int x2, int y2, int roundw, int roundh)
+        : m_x1(x1), m_y1(y1), m_x2(x2), m_y2(y2)
+        , m_roundw(roundw), m_roundh(roundh)
+        , m_region(CreateRoundRectRgn(x1, y1, x2, y2, m_roundw, m_roundh))
+    {}
+
+    WindowRegion& WindowRegion::SetRound(int roundw, int roundh)
+    {
+        updateRound(roundw, roundh);
+        reassign();
+        return *this;
+    }
+
+    WindowRegion& WindowRegion::UnsetRound()
+    {
+        return SetRound(0, 0);
+    }
+
+    WindowRegion& WindowRegion::ResetRegion(int x1, int y1, int x2, int y2)
+    {
+        updateCoord(x1, y1, x2, y2);
+        reassign();
+        return *this;
+    }
+
+    int WindowRegion::ApplyToWindow(HWND hwnd) const
+    {
+        return SetWindowRgn(hwnd, m_region, true);
+    }
+
+    WindowRegion::~WindowRegion()
+    {
+        DeleteObject(m_region);
+    }
+}
+
+
+
 AppRegion::AppRegion(HWND hwnd, int x, int y, POINT min, POINT max)
     : m_hWnd(hwnd)
     , m_min(min)
@@ -7,35 +70,25 @@ AppRegion::AppRegion(HWND hwnd, int x, int y, POINT min, POINT max)
     , m_windowRegion(x, y, m_max.x, m_max.y)
 {}
 
-
-
 int AppRegion::ApplyToWindow() const
 {
     return m_windowRegion.ApplyToWindow(m_hWnd);
 }
-
-
 
 int AppRegion::UnmakeRounding()
 {
     return m_windowRegion.UnsetRound().ApplyToWindow(m_hWnd);
 }
 
-
-
 int AppRegion::MakeRounding(int width, int height)
 {
     return m_windowRegion.SetRound(width, height).ApplyToWindow(m_hWnd);
 }
 
-
-
 int AppRegion::ShowMaxWindow()
 {
     return m_windowRegion.ResetRegion(10, 32, m_max.x, m_max.y).ApplyToWindow(m_hWnd);
 }
-
-
 
 int AppRegion::ShowMinWindow()
 {
--- AppRegion.h
+++ AppRegion.h
@@ -1,13 +1,46 @@
 #pragma once
 
-#include "WindowRegion.h"
+#include <Windows.h>
+
+namespace window_region
+{
+    class WindowRegion {
+    private:
+        int m_x1;
+        int m_y1;
+        int m_x2;
+        int m_y2;
+        int m_roundw;
+        int m_roundh;
+        HRGN m_region{};
+
+    private:
+        void reassign();
+        void updateRound(int roundw, int roundh);
+        void updateCoord(int x1, int y1, int x2, int y2);
+
+    public:
+        WindowRegion(int x1, int y1, int x2, int y2, int roundw = 0, int roundh = 0);
+        WindowRegion(const WindowRegion&) = delete;
+        WindowRegion& operator=(const WindowRegion&) = delete;
+        WindowRegion(WindowRegion&&) = default;
+        WindowRegion& operator=(WindowRegion&&) = default;
+        WindowRegion& SetRound(int roundw, int roundh);
+        WindowRegion& UnsetRound();
+        WindowRegion& ResetRegion(int x1, int y1, int x2, int y2);
+        int ApplyToWindow(HWND hwnd) const;
+        ~WindowRegion();
+    };
+}
+
+
 
 class AppRegion {
 private:
     HWND m_hWnd;
     POINT m_min;
     POINT m_max;
-    WindowRegion m_windowRegion;
+    window_region::WindowRegion m_windowRegion;
 
 public:
     AppRegion(HWND hwnd, int x, int y, POINT min, POINT max);

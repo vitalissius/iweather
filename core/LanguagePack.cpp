402590e407148788311dd7b6c4192eb96decb7d5
Vitalii <vitaliy.sklyarov@gmail.com>
2017-03-07 13:18:24 +0200

    Списки инициализации вместо явного указания типов элементов

--- core/LanguagePack.cpp
+++ core/LanguagePack.cpp
@@ -113,22 +113,22 @@ const std::string& LanguagePack::GetWindLandCondition(units::BeaufortNumbers bea
 
 const std::unordered_map<LanguagePack::Keys, std::vector<std::string>> LanguagePack::csm_engPack
 {
-    std::make_pair<Keys, std::vector<std::string>>(KEY_PRESSURE,{ "atm", "mmHg", "mb", "inHg" }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_SPEED,{ "km/h", "m/s", "mph", "ft/s" }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_TEMPERATURE,{ "C", "F" }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_PRESSURESTATE,{ "Steady", "Rising", "Falling" }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_COMPASSROSE,{
+    { KEY_PRESSURE, { "atm", "mmHg", "mb", "inHg" } },
+    { KEY_SPEED, { "km/h", "m/s", "mph", "ft/s" } },
+    { KEY_TEMPERATURE, { "C", "F" } },
+    { KEY_PRESSURESTATE, { "Steady", "Rising", "Falling" } },
+    { KEY_COMPASSROSE, {
         "North",    "North-northeast",  "Northeast",    "East-northeast",
         "East",     "East-southeast",   "Southeast",    "South-southeast",
         "South",    "South-southwest",  "Southwest",    "West-southwest",
         "West",     "West-northwest",   "Northwest",    "North-northwest"
-    }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_WIND_DESCRIPTION,{
+    } },
+    { KEY_WIND_DESCRIPTION, {
         "Calm", "Light air", "Light breeze", "Gentle breeze",
         "Moderate breeze", "Fresh breeze", "Strong breeze", "High wind",
         "Gale", "Strong gale", "Storm", "Violent storm", "Hurricane"
-    }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_WIND_LANDCONDITION,{
+    } },
+    { KEY_WIND_LANDCONDITION, {
         "Calm. Smoke rises vertically",
         "Smoke drift indicates wind direction. Leaves and wind vanes are stationary",
         "Wind felt on exposed skin. Leaves rustle. Wind vanes begin to move",
@@ -142,27 +142,27 @@ const std::unordered_map<LanguagePack::Keys, std::vector<std::string>> LanguageP
         "Trees are broken off or uprooted, structural damage likely",
         "Widespread vegetation and structural damage likely",
         "Severe widespread damage to vegetation and structures. Debris and unsecured objects are hurled about"
-    }),
+    } },
 };
 
 const std::unordered_map<LanguagePack::Keys, std::vector<std::string>> LanguagePack::csm_rusPack
 {
-    std::make_pair<Keys, std::vector<std::string>>(KEY_PRESSURE,{ "атм", "мм рт.ст.", "мбар", "дюйм рт.ст." }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_SPEED,{ "км/ч", "м/с", "миль/ч", "фт/с" }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_TEMPERATURE,{ "C", "F" }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_PRESSURESTATE,{ "Постоянное", "Повышается", "Понижается" }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_COMPASSROSE,{
+    { KEY_PRESSURE, { "атм", "мм рт.ст.", "мбар", "дюйм рт.ст." } },
+    { KEY_SPEED, { "км/ч", "м/с", "миль/ч", "фт/с" } },
+    { KEY_TEMPERATURE, { "C", "F" } },
+    { KEY_PRESSURESTATE, { "Постоянное", "Повышается", "Понижается" } },
+    { KEY_COMPASSROSE, {
         "Северный",     "Северо-северо-восточный",      "Северо-восточный",     "Востоко-северо-восточный",
         "Восточный",    "Востоко-юго-восточный",       "Юго-восточный",        "Юго-юго-восточный",
         "Южный",        "Юго-юго-западный",             "Юго-западный",         "Западо-юго-западный",
         "Западный",     "Западо-северо-западный",      "Северо-западный",      "Северо-северо-западный"
-    }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_WIND_DESCRIPTION,{
+    } },
+    { KEY_WIND_DESCRIPTION, {
         "Штиль", "Тихий", "Лёгкий", "Слабый",
         "Умеренный", "Свежий", "Сильный", "Крепкий",
         "Очень крепкий", "Шторм", "Сильный шторм", "Жестокий шторм", "Ураган",
-    }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_WIND_LANDCONDITION,{
+    } },
+    { KEY_WIND_LANDCONDITION, {
         "Безветрие. Дым поднимается вертикально, листья деревьев неподвижны",
         "Направление ветра заметно по относу дыма, но не по флюгеру",
         "Движение ветра ощущается лицом, шелестят листья, приводится в движение флюгер",
@@ -176,27 +176,27 @@ const std::unordered_map<LanguagePack::Keys, std::vector<std::string>> LanguageP
         "Значительные разрушения строений, ветер вырывает деревья с корнем",
         "Большие разрушения на значительном пространстве. Наблюдается очень редко",
         "Огромные разрушения, серьезно повреждены здания, строения и дома, деревья вырваны с корнями, растительность уничтожена. Случай очень редкий"
-    }),
+    } },
 };
 
 const std::unordered_map<LanguagePack::Keys, std::vector<std::string>> LanguagePack::csm_ukrPack
 {
-    std::make_pair<Keys, std::vector<std::string>>(KEY_PRESSURE,{ "атм", "мм рт.ст.", "мбар", "дюйм рт.ст." }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_SPEED,{ "км/год", "м/с", "миль/год", "фт/с" }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_TEMPERATURE,{ "C", "F" }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_PRESSURESTATE,{ "Стаціонарний", "Зростаючий", "Спадний" }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_COMPASSROSE,{
+    { KEY_PRESSURE, { "атм", "мм рт.ст.", "мбар", "дюйм рт.ст." } },
+    { KEY_SPEED, { "км/год", "м/с", "миль/год", "фт/с" } },
+    { KEY_TEMPERATURE, { "C", "F" } },
+    { KEY_PRESSURESTATE, { "Стаціонарний", "Зростаючий", "Спадний" } },
+    { KEY_COMPASSROSE, {
         "Північний",    "Північно-північно-східний",   "Північно-східний",     "Східно-північно-східний",
         "Східний",      "Східно-північно-східний",     "Південно-східний",     "Південно-південно-східний",
         "Південний",    "Південно-південно-західний",  "Південно-західний",    "Західно-південно-західний",
         "Західний",     "Західно-північно-західний",   "Північно-західний",    "Північно-північно-західний"
-    }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_WIND_DESCRIPTION,{
+    } },
+    { KEY_WIND_DESCRIPTION, {
         "Штиль", "Тихий", "Легкий", "Слабкий",
         "Помірний", "Свіжий", "Сильний", "Міцний",
         "Дуже міцний", "Шторм", "Сильний шторм", "Жорстокий шторм", "Ураган",
-    }),
-    std::make_pair<Keys, std::vector<std::string>>(KEY_WIND_LANDCONDITION,{
+    } },
+    { KEY_WIND_LANDCONDITION, {
         "Повна відсутність вітру. Дим піднімається прямовисно. Листя дерев нерухомі",
         "Дим «пливе». Флюгер не обертається",
         "Рух повітря відчувається обличчям. Шелестить листя. Флюгер обертається",
@@ -210,5 +210,5 @@ const std::unordered_map<LanguagePack::Keys, std::vector<std::string>> LanguageP
         "Значні руйнування. Дерева вириваються з корінням",
         "Великі руйнування",
         "Призводить до спустошень"
-    }),
+    } },
 };
